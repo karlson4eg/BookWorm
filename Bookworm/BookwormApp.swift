b158ab38906e3ef1e3b03a0535bdb70d5f61f53c
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-15 19:16:48 +0400

    storing books in core data

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Evi St on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    @StateObject private var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Evi St on 4/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationView{
+            Text("Count is: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddScreen.toggle()
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
