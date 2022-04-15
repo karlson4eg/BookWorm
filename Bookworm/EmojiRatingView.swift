ff8da0ea460a2f4482c1e23fb2008e6d8a5a3033
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-15 19:58:00 +0400

    added list of stored books that is being built by @FetchRequest

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,37 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Evi St on 4/15/22.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("🤢")
+        case 2:
+            return Text("👎")
+        case 3:
+            return Text("😐")
+        case 4:
+            return Text("👍")
+        case 5:
+            return Text("🥰")
+        default:
+            return Text("😐")
+        }
+            
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
