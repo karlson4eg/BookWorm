90048201be54f8eabd33ba4a68f76dd3b0ab9a75
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-25 20:33:40 +0400

    accessibility actions added to RatingVIew

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -35,6 +35,20 @@ struct RatingView: View {
                     }
             }
         }
+        .accessibilityElement()
+        .accessibilityLabel("Rating")
+        .accessibilityValue(rating == 1 ? "One star" : "\(rating) stars")
+        .accessibilityAdjustableAction { direction in
+            switch direction{
+            case .increment:
+                if rating < maximumRating { rating += 1 }
+            case .decrement:
+                if rating > 1 { rating -= 1}
+            default:
+                break
+            }
+            
+        }
     }
     
     func image(for number: Int) -> Image {
